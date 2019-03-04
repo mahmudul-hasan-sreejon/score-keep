2716de4e2b5c98083127aa162c1d0f6e8fc6b39e
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-05 01:23:35 +0600

    handleSubmit as method added

--- imports/ui/AddPlayer.js
+++ imports/ui/AddPlayer.js
@@ -1,33 +1,37 @@
 
 import React from 'react';
 
+import {Players} from './../api/players';
 
-const handleSubmit = e => {
-  // Stop page refresh
-  e.preventDefault();
-
-  const playerName = e.target.playerName.value;
-  if(playerName) {
-    // Clear input field
-    e.target.playerName.value = '';
-
-    // Insert new players data
-    Players.insert(
-      {
-        name: playerName,
-        score: 0
-      }
-    );
-  }
-};
 
 export default class AddPlayer extends React.Component {
+  handleSubmit(e) {
+    // Stop page refresh
+    e.preventDefault();
+
+    const playerName = e.target.playerName.value;
+    if(playerName) {
+      // Clear input field
+      e.target.playerName.value = '';
+
+      // Insert new players data
+      Players.insert(
+        {
+          name: playerName,
+          score: 0
+        }
+      );
+    }
+  }
+
 	render() {
 		return (
-			<form onSubmit={handleSubmit}>
-				<input type='text' name='playerName' placeholder='Player Name' />
-				<button>Add Player</button>
-			</form>
+      <div>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input type='text' name='playerName' placeholder='Player Name' />
+          <button>Add Player</button>
+        </form>
+      </div>
 		);
 	}
 }
