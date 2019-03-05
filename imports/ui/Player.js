2d088ddad397f5a1a1226a5f75adea3ece4973b9
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-06 01:43:01 +0600

    Player rank ui updated

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -10,13 +10,17 @@ export default class Player extends React.Component {
     const playerId = this.props.player._id;
     const playerName = this.props.player.name;
     const playerScore = this.props.player.score;
+    const playerRank = this.props.player.rank;
+    const playerPosition = this.props.player.position;
+
+    const itemClassName = `item item--position-${playerRank}`;
 
     return (
-        <div key={playerId} className='item'>
+        <div key={playerId} className={itemClassName}>
           <div className='player'>
             <div>
               <h3 className='player__name'>{playerName}</h3>
-              <p className='player__stats'>{playerScore} point(s)</p>
+              <p className='player__stats'>{playerPosition} place - {playerScore} point(s)</p>
             </div>
             
             <div className='player__actions'>
